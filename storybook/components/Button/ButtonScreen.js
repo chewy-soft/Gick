892cbd97a3fa95e8effb057933038edf21e76a40
nana yang <roymunou7@yahoo.co.jp>
2019-03-25 09:20:31 +0800

    [[git-now]] Mon Mar 25 09:20:31 DST 2019
     storybook/components/Button/ButtonScreen.js        | 128 ++++++++++-----------
     .../components/Button/examples/PropHalfTheme.js    |  30 -----
     storybook/components/Button/examples/PropTheme.js  |  44 -------
     3 files changed, 58 insertions(+), 144 deletions(-)

--- storybook/components/Button/ButtonScreen.js
+++ storybook/components/Button/ButtonScreen.js
@@ -1,72 +1,60 @@
-import React from "react";
-import { View } from "react-native";
-import PageContainer from "../../sampleUI/PageContainer";
-import PageTitle from "../../sampleUI/PageTitle";
-import { Example, ExampleDark } from "../../sampleUI/Example";
-import Code from "../../sampleUI/Code";
-import Description from "../../sampleUI/Description";
-import PropTheme from "./examples/PropTheme";
-import PropHalfTheme from "./examples/PropHalfTheme";
-import PropSize from "./examples/PropSize";
-import OrderButton from "../../../src/components/Buttons/OrderButton";
-import Button from "../../../src/components/Buttons/Button";
+import React from 'react';
+import { View } from 'react-native';
+import PageContainer from '../../sampleUI/PageContainer';
+import PageTitle from '../../sampleUI/PageTitle';
+import { Example, ExampleDark } from '../../sampleUI/Example';
+import Code from '../../sampleUI/Code';
+import Description from '../../sampleUI/Description';
+import { DividerHorizontal, DividerVertical } from '../../sampleUI/helps';
+import PropSize from './examples/PropSize';
+import OrderButton from '../../../src/components/Buttons/OrderButton';
+import Button from '../../../src/components/Buttons/Button';
+import { themes } from '../../../src/components/Buttons/styles/Button/styles.default';
+import _ from 'lodash';
 
-export default props => (
-  <PageContainer>
-    <PageTitle title="Buttons" />
-    <Description info="Button Colors" />
-    <ExampleDark
-      example={
-        <View>
-          <Code>
-            {"<Button theme='orange' content='orange' onPress='...' />"}
-          </Code>
-          <PropTheme />
-        </View>
-      }
-    />
-    <Example
-      example={
-        <View>
-          <Code>
-            {"<Button theme='halfOrange' content='orange' onPress='...' />"}
-          </Code>
-          <PropHalfTheme />
-        </View>
-      }
-    />
-    <Description info="Button Size S" />
-    <Example
-      example={
-        <View>
-          <Code>
-            {"<Button theme='darkBlue' content='...' onPress='...' />"}
-          </Code>
-          <PropSize />
-        </View>
-      }
-    />
-    <Description info="Button disabled,Boolean。" />
-    <ExampleDark
-      example={
-        <View>
-          <Code>{'<Button disabled content="Disabled button" />'}</Code>
-          <Button disabled content="Disabled button" />
-        </View>
-      }
-    />
-    <Description info="Example Order Button" />
-    <ExampleDark
-      example={
-        <View>
-          <Code>{'<OrderButton order="unset" />'}</Code>
-          <OrderButton order="unset" />
-          <Code>{'<OrderButton order="up" />'}</Code>
-          <OrderButton order="up" />
-          <Code>{'<OrderButton order="down" />'}</Code>
-          <OrderButton order="down" />
-        </View>
-      }
-    />
-  </PageContainer>
+export default (props) => (
+	<PageContainer>
+		<PageTitle title="Buttons" />
+		<Description info="Button Colors" />
+		<ExampleDark
+			example={_.map(themes, (m, index) => (
+				<View key={index}>
+					<Code>{`<Button theme='${index}' content='${index}' onPress='...' />`}</Code>
+					<Button theme={index} content={index} icon="qq" />
+					<DividerHorizontal />
+				</View>
+			))}
+		/>
+		<Description info="Button Size S" />
+		<Example
+			example={
+				<View>
+					<Code>{"<Button theme='darkBlue' content='...' onPress='...' />"}</Code>
+					<PropSize />
+				</View>
+			}
+		/>
+		<Description info="Button disabled,Boolean。" />
+		<ExampleDark
+			example={
+				<View>
+					<Code>{'<Button disabled content="Disabled button" />'}</Code>
+					<Button disabled content="Disabled button" />
+				</View>
+			}
+		/>
+		<Description info="Example Order Button" />
+		<ExampleDark
+			example={
+				<View>
+					<Code>{'<OrderButton order="unset" />'}</Code>
+					<OrderButton order="unset" />
+					<Code>{'<OrderButton order="up" />'}</Code>
+					<OrderButton order="up" />
+					<Code>{'<OrderButton order="down" />'}</Code>
+					<OrderButton order="down" />
+				</View>
+			}
+		/>
+	</PageContainer>
 );
